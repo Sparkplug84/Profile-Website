118fe7021a48b5be4058236fc693001ee7581ffc
Mark McClean <mcclean_mark_02@hotmail.com>
2024-06-25 23:49:44 +0200

    backgrond landing animation added

--- src/components/LandingSection.vue
+++ src/components/LandingSection.vue
@@ -1,12 +1,14 @@
 <script setup>
 import PrimaryButton from './PrimaryButton.vue'
 import SecondayButton from './SecondaryButton.vue'
+import BackgroundAnimation from './BackgroundAnimation.vue'
 </script>
 
 <template>
   <div class="flex items-center h-screen w-screen text-white">
     <img src="../assets/landingImage.jpg" alt="" class="h-full w-full object-cover" />
-    <div class="absolute h-screen w-screen bg-black opacity-75"></div>
+    <!-- <div class="absolute h-screen w-screen bg-black opacity-75"></div> -->
+    <background-animation class="absolute h-screen w-screen opacity-75" />
     <div class="absolute left-1/4">
       <p class="text-2xl opacity-0 animate-fadeInLeft animation-delay-1s">Web Developer</p>
       <img
